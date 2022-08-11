0f27eac9df43dbfbb96d0ab653cf767a5dce2686
erick <erickrrochaa1999@gmail.com>
2022-08-11 17:33:42 -0300

    Main/Settings

--- BNDESIntegration/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Transaction, Company, BNDESTransaction, ArchiveBNDESTransaction
-
-# Register your models here.
-admin.site.register(
-    [Transaction, Company, BNDESTransaction, ArchiveBNDESTransaction])
